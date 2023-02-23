a524aa4067f71454086b95e8a00150fdddc9d171
Vladislav Lekontsev <other2other2@gmail.com>
2023-02-23 18:39:47 +0400

    feat: solve the exercise

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max - this.min) / 2) + this.min;
     }
 
     lower() {
-
+        this.max = this.guess();
+        console.log('min: ' + this.min + '    max: ' + this.max);
     }
 
     greater() {
-
+        this.min = this.guess();
+        console.log('min: ' + this.min + '    max: ' + this.max);
     }
 }
 
 module.exports = GuessingGame;
+
